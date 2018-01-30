6501b3897096dd388d62739c1993bc4df497df1a
kei.yamaguchi <kei.yamaguchi@ERSH-E0390.eurus.local>
2018-01-30 13:55:08 +0900

    初コミット

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+//コメント
+#コメント
+
+echo "hello from the TOP! ";
+
+/*
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+ <p>hello world <?php  echo "from PHP"; ?></p>
+</body>
+</html>
