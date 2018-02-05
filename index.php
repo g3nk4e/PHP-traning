a73a36e5e59d352473ba1f0e973f9d24cd810c9e
kei.yamaguchi <kei.yamaguchi@ERSH-E0390.eurus.local>
2018-02-05 11:47:06 +0900

    練習中

--- index.php
+++ index.php
@@ -13,7 +13,42 @@ PHPの勉強をしていきます。
 <br><br>
 <?php 
 
-echo "5 * 6 = ",5 * 6;
+$scpre = 60;
+
+if ($scpre >= 60) {
+	echo "Greate!!!<br />";
+	} elseif ($scpre >= 50) {
+echo "Good<br />";
+} elseif  ($scpre >= 40){
+echo "もっと勉強しろ<br />";
+}
+
+$a = 5;
+$b = 14;
+
+$max = ($a > $b) ? $a : $b;
+
+echo "$max<br/>" ;
+ 
+$domain = 'cn';
+
+echo 'ドメイン:' .$domain.'<br/>';
+
+switch ($domain){
+	case 'in':
+		echo 'インドのドメイン';
+		break;
+	case 'cn':
+		echo '中国のドメイン';
+		break;
+	case 'jp':
+		echo '日本のドメイン';
+		break;
+	default:
+		print '不明です';
+}
+
+
 
 ?>
 
